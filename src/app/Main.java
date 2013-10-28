223f0a775308d7b34a1410974551f5916f67cfa0
Vincent Chu <vince.chu@live.ca>
2013-10-28 00:56:37 -0400

    Code cleanup: WindowListener to WindowAdapter in Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -22,14 +22,10 @@ public class Main {
     	UIToolbox.fullscreen(frame);
     	frame.setExtendedState(Frame.MAXIMIZED_BOTH);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.addWindowListener(new WindowListener() {
-			@Override public void windowOpened(WindowEvent e) {}
-			@Override public void windowIconified(WindowEvent e) {}
-			@Override public void windowDeiconified(WindowEvent e) {}
-			@Override public void windowDeactivated(WindowEvent e) {}
-			@Override public void windowClosed(WindowEvent e) {}
-			@Override public void windowActivated(WindowEvent e) {}
-			@Override public void windowClosing(WindowEvent e) {DBManager.SELF.destroy();}
+        frame.addWindowListener(new WindowAdapter() {
+			@Override public void windowClosing(WindowEvent e) {
+				DBManager.SELF.destroy();
+			}
 		});
     	frame.setLayout(MultiPanel.SELF.setParent(frame));
     		MultiPanel.SELF.add(new WelcomePage());	
